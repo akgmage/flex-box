3c4719807089e09d4fc5d5b41faf4caee7986dc3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-24 08:57:34 +0530

    remove declaration of header and footer from shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-
-
 
 @NgModule({
-  declarations: [
-    FooterComponent,
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [],
+  imports: [CommonModule],
 })
-export class SharedModule { }
+export class SharedModule {}
