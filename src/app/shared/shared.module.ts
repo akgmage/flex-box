7736a154bacda395daadf935536d17d8290f6f24
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-24 08:59:47 +0530

    add common component in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CommonComponent } from './common/common.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CommonComponent
+  ],
   imports: [CommonModule],
 })
 export class SharedModule {}
