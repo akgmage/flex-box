c1ce31af4b9210934e8c0ee0c6eab727bfd5d255
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-07-25 08:33:24 +0530

    add side nav inside shared component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CommonComponent } from './common/common.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    CommonComponent
+    CommonComponent,
+    SidenavComponent
   ],
   imports: [CommonModule],
 })
