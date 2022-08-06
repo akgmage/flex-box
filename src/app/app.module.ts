f183901e715c7fff0070d2a9778cb6bf7efda504
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-06 23:06:51 +0530

    remove unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
 import { FigureCaptionComponent } from './figure-caption/figure-caption.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ContactComponent, AboutComponent, FigureCaptionComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    FigureCaptionComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
