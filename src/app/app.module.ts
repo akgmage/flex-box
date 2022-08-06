78e4a0869df1e81ae5cea6fa1ca2ef90b30b4420
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-06 23:11:38 +0530

    remove contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 import { FigureCaptionComponent } from './figure-caption/figure-caption.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    ContactComponent,
-    FigureCaptionComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, FigureCaptionComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
